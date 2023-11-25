995390891109ad7d2558f11ad965ab2a49e6af8a
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 17:14:55 +0100

    fix

--- serverless/api.js
+++ serverless/api.js
@@ -12,13 +12,13 @@ app.set('view engine', 'pug')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-router.get('/pug', (req, res) => {
+router.get('/', (req, res) => {
     res.send(pug.renderFile(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' }))
     // res.render(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' })
 })
 
 router.get("/hello", (req, res) => res.send("Hello world"))
 
-app.use("/.netlify/functions/api", router)
+app.use("/.netlify/functions/", router)
 
 exports.handler = serverless(app)
