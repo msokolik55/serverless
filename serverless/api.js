6c95f47346d8994a11151eb29d9061d579c4940a
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 17:13:04 +0100

    fix

--- serverless/api.js
+++ serverless/api.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/pug', (req, res) => {
-    // res.send(pug.renderFile(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' }))
-    res.render(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' })
+    res.send(pug.renderFile(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' }))
+    // res.render(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' })
 })
 
 router.get("/hello", (req, res) => res.send("Hello world"))
