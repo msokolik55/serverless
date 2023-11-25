f1c8ac49706d78750e1d9c0a45a866b08521cbfa
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 12:52:37 +0100

    feat: added render to "/"

--- serverless/api.js
+++ serverless/api.js
@@ -4,6 +4,7 @@ const serverless = require("serverless-http")
 const api = express()
 
 const router = express.Router()
+router.get("/", (req, res) => res.render("<h1>Hallo</h1>"))
 router.get("/hello", (req, res) => res.send("Hello world"))
 
 api.use("/.netlify/functions/api/", router)
