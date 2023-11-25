d8fc131166248c37d5e014a196bf69b3512801b2
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 12:58:46 +0100

    feat: added index.pug

--- serverless/api.js
+++ serverless/api.js
@@ -2,8 +2,14 @@ const express = require("express")
 const serverless = require("serverless-http")
 
 const api = express()
-
 const router = express.Router()
+
+api.set('view engine', 'pug')
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
 router.get("/hello", (req, res) => res.send("Hello world"))
 
 api.use("/.netlify/functions/api/", router)
