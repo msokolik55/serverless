0b9ba0c12af5af33c405fae5543847dad41833c8
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 17:11:46 +0100

    fix

--- serverless/api.js
+++ serverless/api.js
@@ -3,26 +3,18 @@ const serverless = require("serverless-http")
 const path = require("path")
 const bodyParser = require("body-parser")
 const pug = require("pug")
-const fs = require("fs")
 
 const app = express()
 const router = express.Router()
 
-// app.engine("pug", pug.__express)
 app.set('view engine', 'pug')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/pug', (req, res) => {
-    // res.send(pug.render("h1= title", { title: "hello" }))
-    fs.readdirSync(__dirname).forEach(file => {
-        console.log(file);
-    })
-    console.log(pug.renderFile(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' }))
     // res.send(pug.renderFile(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' }))
-
-    // res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render(path.join("views", "index.pug"), { title: 'Hey', message: 'Hello there!' })
 })
 
 router.get("/hello", (req, res) => res.send("Hello world"))
