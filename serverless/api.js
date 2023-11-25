234ee71257c4be7f44f2f07427e0aed49678e7c8
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 12:54:46 +0100

    fix: removed render

--- serverless/api.js
+++ serverless/api.js
@@ -4,7 +4,6 @@ const serverless = require("serverless-http")
 const api = express()
 
 const router = express.Router()
-router.get("/", (req, res) => res.render("<h1>Hallo</h1>"))
 router.get("/hello", (req, res) => res.send("Hello world"))
 
 api.use("/.netlify/functions/api/", router)
