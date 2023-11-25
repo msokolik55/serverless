952982335abd83c4e3bcd477eba3ca8ee9de7fc9
Michal Sokolik <msokolik55@gmail.com>
2023-11-25 13:59:23 +0100

    fix: remove comment

--- serverless/api.js
+++ serverless/api.js
@@ -7,7 +7,7 @@ const app = express()
 const router = express.Router()
 
 app.engine("pug", require("pug").__express)
-// app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(bodyParser.json())
